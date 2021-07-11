35b4d56a393e511913af5e94b84a20e05204de1a
Carlos Estrada Ceballos <carloschivas@fb.com>
2021-07-11 15:15:19 -0500

    User can login

--- app/src/main/java/com/example/zapofood/LoginActivity.java
+++ app/src/main/java/com/example/zapofood/LoginActivity.java
@@ -2,11 +2,15 @@ package com.example.zapofood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
@@ -29,12 +33,30 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String username = etLoginUsername.getText().toString();
                 String password = etLoginPassword.getText().toString();
-                //loginUser(username, password);
+                loginUser(username, password);
             }
         });
     }
 
-    /*private void loginUser(String username, String password){
-        ParseUser.lo
-    }*/
+    private void loginUser(String username, String password){
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    Toast.makeText(LoginActivity.this, "Error with Login!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //Navigate to the main activity if the user has signed in properly
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Succes!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    //Go to the main activity
+    private void goMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
